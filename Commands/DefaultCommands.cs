a0f289f24080ad75636977bf2799991bb7c86ebe
Haohan Liu <the1nonlypanda@gmail.com>
2020-02-07 07:46:23 +1100

    Add project files.

---
+++ Commands/DefaultCommands.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shelleton.Commands
+{
+    public static class DefaultCommands
+    {
+        public static string Print(string text)
+        {
+            return string.Format("Printed '{0}'", text);
+        }
+
+        public static string Date(DateTime date)
+        {
+            return string.Format("Date: {0}", date);
+        }
+
+        public static string Optional(int id, string data = "No Data Provided")
+        {
+            var result = string.Format(
+                "I did something to the record Id {0} and save the data {1}", id, data);
+
+            if (data == "No Data Provided")
+            {
+                result = string.Format(
+                "I did something to the record Id {0} but the optinal parameter "
+                + "was not provided, so I saved the value '{1}'", id, data);
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shelleton
+{
+    // Code adapted from http://johnatten.com/2014/09/07/c-building-a-useful-extensible-net-console-application-template-for-development-and-testing/
+    /* 
+        1. Methods representing Console commands will be defined as public static methods which always return a string, and are defined on public static classes.
+    
+        2. Classes containing methods representing Console commands will be located in the Commands namespace, and in the Commands folder.
+    
+        3. There will always be a static class named DefaultCommands from which methods may be invoked from the Console directly by name. For many console project, this will likely be sufficient.
+    
+        4. Commands defined on classes other than DefaultCommands will be invoked from the console using the familiar dot syntax: ClassName.CommandName.
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = typeof(Program).Name;
+
+            Shell.Start();
+
+            Console.WriteLine("Program will terminate...");
+            Console.ReadKey();
+        }
+    }
+}
